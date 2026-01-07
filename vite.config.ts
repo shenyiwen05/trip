60f9b29e012092e0dc353ce9c13bfd8a38ea8db9
shenyiwen05 <649973667@qq.com>
2026-01-07 17:15:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: '/trip/',  // <--- 这里加了逗号，非常重要！
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
